5865cb4b475d0e509f39a7fa381823864af3f028
Wessel van Dorsten <wesselvandorsten@gmail.com>
2024-08-09 12:48:58 +0200

    models

---
+++ Rommulbad/Domain/Guardian.fs
@@ -0,0 +1,38 @@
+﻿module Rommulbad.Domain.Guardian
+
+open System.Text.RegularExpressions
+open Thoth.Json.Net
+open Rommulbad.Domain.Candidate
+
+type Guardian =
+    { Id: string
+      Name: string
+      Candidates: List<Candidate> }
+
+module Guardian =
+    let encode: Encoder<Guardian> =
+        fun guardian ->
+            Encode.object
+                [ "id", Encode.string guardian.Id
+                  "name", Encode.string guardian.Name
+                  "candidates", guardian.Candidates |> List.map Candidate.encode |> Encode.list ]
+
+    let decode: Decoder<Guardian> =
+        Decode.object (fun get ->
+            { Id = get.Required.Field "id" Decode.string
+              Name = get.Required.Field "name" Decode.string
+              Candidates = get.Required.Field "candidates" (Decode.list Candidate.decode) })
+
+    let private hasOnlyLettersAndSpaces (name: string) = Regex.IsMatch(name, @"^[A-Za-z\s]+$")
+
+    let private hasNoMoreThanTwoConsecutiveSpaces (name: string) = Regex.IsMatch(name, @"\s{2,}")
+
+    let private hasCapitalizedFirstLetters (name: string) = Regex.IsMatch(name, @"[A-Z]")
+
+    let isValidGuardianName (name: string) =
+        hasOnlyLettersAndSpaces name
+        && hasNoMoreThanTwoConsecutiveSpaces name
+        && hasCapitalizedFirstLetters name
+
+    let isValidGuardianId (id: string) =
+        Regex.IsMatch(id, @"^\d{3}-[A-Za-z]{4}$")
---
+++ Rommulbad/Domain/Session.fs
@@ -0,0 +1,25 @@
+﻿module Rommulbad.Domain.Session
+
+open System
+open Thoth.Json.Net
+
+type Session =
+    { Deep: bool
+      Date: DateTime
+      Minutes: int }
+
+module Session =
+    let encode: Encoder<Session> =
+        fun session ->
+            Encode.object
+                [ "deep", Encode.bool session.Deep
+                  "date", Encode.datetime session.Date
+                  "amount", Encode.int session.Minutes ]
+
+    let decode: Decoder<Session> =
+        Decode.object (fun get ->
+            { Deep = get.Required.Field "deep" Decode.bool
+              Date = get.Required.Field "date" Decode.datetime
+              Minutes = get.Required.Field "amount" Decode.int })
+        
+    let hasValidMinutes (minutes: int) = minutes >= 0 && minutes <= 30
